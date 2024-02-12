2e6bd2be58cd969342a61a0708b42c4ee3b0f6f7
Gulrez Alam <egulrezalam@gmail.com>
2024-02-12 20:27:42 +0530

    Update build.sh and home.html templates

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER == "True" ]];
+then
+  python manage.py createsuperuser --no-input
+fi
